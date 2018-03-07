45c3b8ea164af0b947c557b15b61b4c2ef22d523
rawalshree <rawalshreepal000@gmail.com>
2018-03-06 23:43:57 -0800

    three rotor enigma

--- threerotorenigma.py
+++ threerotorenigma.py
@@ -10,12 +10,14 @@ from collections import OrderedDict
 from itertools import islice, cycle
 global plain
 global cipher
+global Success
 global position1
 global position2
 global position3
 global counter1
 global counter2
 global counter3
+Success = False
 counter1 = 0
 counter2 = 0
 counter3 = 0
@@ -41,60 +43,77 @@ class ThreeRotorEnigma:
         )
 
     def setKey(self, key):
+        global Success
         self.key = key
+        self.key = " ".join(self.key.split())
+        
         key1, key2, key3 = self.key.split(" ")
 
-        for x in range(26):
-            position1[chr(x+97)] = key1[x]
+        if len(key1) == 26:
+            for x in range(26):
+                position1[chr(x+97)] = key1[x]
         
-        for x in range(26):
-            position2[chr(x+97)] = key2[x]
+        if len(key2) == 26:
+            for x in range(26):
+                position2[chr(x+97)] = key2[x]
 
-        for x in range(26):
-            position3[chr(x+97)] = key3[x]
+        if len(key3) == 26:
+            for x in range(26):
+                position3[chr(x+97)] = key3[x]
+        
+        if len(key1) + len(key2) + len(key3) == 78 and key1.isalpha() and key2.isalpha() and key3.isalpha():
+            Success = True
             
 
     def encryption(self, plainText):
-        global cipher, position1, position2, position3, counter1, counter2, counter3
+        global cipher, position1, position2, position3, counter1, counter2, counter3, Success
         self.plainText = plainText
 
-        for char in self.plainText:
-            cipher += position3[position2[position1[char]]]
-            counter3 += 1
-            position3 = self.shift_dict(position3, 1)
-            if counter3 == 26:
-                counter2 += 1
-                position2 = self.shift_dict(position2, 1)
-                counter3 = 0
-            if counter2 == 26:
-                counter1 += 1
-                position1 = self.shift_dict(position1, 1)
-                counter2 = 0
-            if counter1 == 26:
-                counter1 = 0
+        if Success:
+            for char in self.plainText:
+                cipher += position3[position2[position1[char]]]
+                counter3 += 1
+                position3 = self.shift_dict(position3, 1)
+                if counter3 == 26:
+                    counter2 += 1
+                    position2 = self.shift_dict(position2, 1)
+                    counter3 = 0
+                if counter2 == 26:
+                    counter1 += 1
+                    position1 = self.shift_dict(position1, 1)
+                    counter2 = 0
+                if counter1 == 26:
+                    counter1 = 0
 
-        return cipher
+            return cipher
+        else:
+            print("Invalid Key")
+            return self.plainText
         
         
     def decryption(self, cipherText):
-        global plain, position1, position2, position3, counter1, counter2, counter3
+        global plain, position1, position2, position3, counter1, counter2, counter3, Success
         self.cipherText = cipherText
 
-        for char in self.cipherText:
-            z = position3.keys()[position3.values().index(char)]
-            z = position2.keys()[position2.values().index(z)]
-            plain += position1.keys()[position1.values().index(z)]
-            counter3 += 1
-            position3 = self.shift_dict(position3, 1)
-            if counter3 == 26:
-                counter2 += 1
-                position2 = self.shift_dict(position2, 1)
-                counter3 = 0
-            if counter2 == 26:
-                counter1 += 1
-                position1 = self.shift_dict(position1, 1)
-                counter2 = 0
-            if counter1 == 26:
-                counter1 = 0
+        if Success:
+            for char in self.cipherText:
+                z = position3.keys()[position3.values().index(char)]
+                z = position2.keys()[position2.values().index(z)]
+                plain += position1.keys()[position1.values().index(z)]
+                counter3 += 1
+                position3 = self.shift_dict(position3, 1)
+                if counter3 == 26:
+                    counter2 += 1
+                    position2 = self.shift_dict(position2, 1)
+                    counter3 = 0
+                if counter2 == 26:
+                    counter1 += 1
+                    position1 = self.shift_dict(position1, 1)
+                    counter2 = 0
+                if counter1 == 26:
+                    counter1 = 0
 
-        return plain
+            return plain
+        else:
+            print("Invalid Key")
+            return self.cipherText
